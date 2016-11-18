5c62e6ba7b69110948616b76af8db63f487966f4
Pille Ulmas <pilleulmas@gmail.com>
2016-11-18 16:50:47 +0200

    Kodukas tegelikult yles

---
+++ main.js
@@ -0,0 +1,21 @@
+var myName = "Tere tulemast!";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, purple, blue, green, orange];
+
+drawName(myName, letterColors);
+
+if(5 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
